5b24130b2c8a0e774af9f3af7b8c64dd918dc8e4
JalenBullock <JalenB846@gmail.com>
2020-07-20 14:49:55 +0000

    The Vowels

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-name = str(input("name..."))
-lowerName = name.lower()
+V = ("a", "e","i", "o", "u")
 
-prrint(lowerName)
+I = str(input("the word is..."))
+
+uI = I.lower()
+
+print(uI)
+
+for i in range(len(I)):
+  if I[i] in V:
+   print(I[i])
+   
+   # of Vovels: 1
+   # of Consonants: 2
+
+A | Vowel
+S | Consonant
+J | Consonant
+W | Consonant
+4 | number 
+2 | number
+# | special character 
+# | special character
+] | special character
+. | special character
+
